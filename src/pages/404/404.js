40782116e4c8e197eb980a6aabb121bb2a460b38
Alexandr Kvasnikov <videot4pe@yandex.ru>
2021-07-12 03:27:48 +0300

    добавил к window application; убрал IIFE (теперь module); добавил scripts для более четкой структуры (в том числе инициализации полей); убрал лишние обращения к DOM, добавил подписку и отписку на события (внутри компонентов)

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,7 +1,10 @@
 import './404.scss';
 
 (function () {
-    window.backToMain = function() {
-        window.location = '/'
+    window.application = {}
+    window.application.errorPage = {
+        backToMain: function() {
+            window.location = '/'
+        }
     }
 })()
--- src/signin.js
+++ src/signin.js
@@ -1,15 +1,5 @@
+import signin from './scripts/signin';
 import './signin.scss';
 
-(function () {
-    window.signIn = function() {
-        const login = document.getElementById('Логин').value
-        const password = document.getElementById('Пароль').value
-        console.log(`Логин: ${login}, пароль: ${password}`)
-        
-        window.location = '/'
-    }
-
-    window.redirectToSignUp = function() {
-        window.location = '/signup.html'
-    }
-})()
+window.application = {}
+window.application.signinModule = signin
