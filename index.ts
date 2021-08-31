c9962ea4ff18181d40947067b7a80d8fd1b1df94
Alexandr Kvasnikov <videot4pe@yandex.ru>
2021-09-01 02:41:21 +0300

    index ts fix

--- index.ts
+++ index.ts
@@ -7,20 +7,8 @@ const app = express();
 
 app.use(express.static('./dist/'));
 
-app.get('/', (_, res) => {
-	res.render('dist/app.html');
-});
-app.get('/main', (_, res) => {
-	res.render('dist/index.html');
-});
-app.get('/signin', (_, res) => {
-	res.render('dist/signin.html');
-});
-app.get('/signup', (_, res) => {
-	res.render('dist/signup.html');
-});
 app.use('*', (_, res) => {
-	res.sendFile(path.join(__dirname, '/dist/404.html'));
+	res.sendFile(path.join(__dirname, '/dist/app.html'));
 });
 
 const port = process.env.PORT ?? 3000;
