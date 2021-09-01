f99e5946dd7ffe58a4d981442fda33a96fe75069
Alexandr Kvasnikov <videot4pe@yandex.ru>
2021-09-01 21:05:18 +0300

    express routes

--- index.ts
+++ index.ts
@@ -5,13 +5,10 @@ dotenv.config({path: __dirname + '/.env'});
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/', (_, res) => {
-	res.sendFile(path.join(__dirname, 'dist/app.html'));
-});
+app.use(express.static('./dist/'));
 
-app.get('*', (_, res) => {
-	res.sendFile(path.join(__dirname, 'dist/app.html'));
+app.use('*', (_, res) => {
+	res.sendFile(path.join(__dirname, '/dist/app.html'));
 });
 
 const port = process.env.PORT ?? 3000;
